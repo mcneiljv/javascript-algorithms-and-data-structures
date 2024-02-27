51c82394ab2e2f79c22fce20667c3e76a9425ad5
Jamie McNeil <jamievmcneil@gmail.com>
2024-02-26 19:04:09 -0500

    Basic setup for Fibonacci problem

---
+++ src/FibonacciSequence.tsx
@@ -0,0 +1,10 @@
+// Problem - Given a number 'n', find the first 'n' elements of the Fibonacci sequence
+// In mathematics, the Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones
+
+// Input - FibonacciSequence(6)
+// Output - [0, 1, 1, 2, 3, 5]
+
+// Notes:
+//
+
+function FibonacciSequence(n: number) {}
