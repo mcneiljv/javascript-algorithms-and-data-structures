6a44b2deb99429ac2007163429baeaa89db32e95
Jamie McNeil <jamievmcneil@gmail.com>
2024-03-12 22:01:31 -0400

    Begin LinkedList implementation

---
+++ src/DataStructures/LinkedList.ts
@@ -0,0 +1,58 @@
+// @ts-nocheck
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log('List is empty');
+    } else {
+      let curr = this.head;
+      let listValues = '';
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log('List values:', listValues);
+    }
+  }
+}
+
+const list = new LinkedList();
+list.prepend(10);
+list.print();
+list.prepend(20);
+list.print();
+list.prepend(30);
+list.print();
+
+console.log('List is empty', list.isEmpty());
+console.log('List size', list.getSize());
